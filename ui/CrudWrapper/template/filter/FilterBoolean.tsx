9f431a175bf6e5073ba87d6059092bfe0329f5fa
Wahyu Aji <wahyuaji0098@gmail.com>
2020-01-14 18:00:25 +0700

    oke

--- ui/CrudWrapper/template/filter/FilterBoolean.tsx
+++ ui/CrudWrapper/template/filter/FilterBoolean.tsx
@@ -8,40 +8,19 @@ export default observer(({ label, field, value, setValue, submit }: any) => {
         oldval: value
     })
     const stackTokens: IStackTokens = { childrenGap: 10 };
-
-    const ToggleBasicExample: React.FunctionComponent = () => {
+    label={label}
+    field={field}
     return (
-    <Stack tokens={stackTokens}>
-      <Toggle label="Enabled and checked" defaultChecked onText="On" offText="Off" onChange={(e: any) => {
-                meta.oldval = e.target.value;
-            }} />
+    <Stack
+        tokens={stackTokens}>
+        <Toggle label="Enabled and checked" onText="On" offText="Off" onChange={          
+            (e: any) => {
+            meta.oldval = e.target.value;
+            setValue=value
+            onclose=submit()
+            value={value}
+            }
+        } />
     </Stack>
   );
-};
-
-function _onChange(ev: React.MouseEvent<HTMLElement>, checked: boolean) {
-  console.log('toggle is ' + (checked ? 'checked' : 'not checked'));
-}
-    return <ItemButton
-        label={label}
-        field={field}
-        onClose={() => submit()}
-        value={value}>
-        {/* <TextField
-            value={meta.oldval}
-            onChange={(e: any) => {
-                meta.oldval = e.target.value;
-            }}
-            onKeyDown={(e: any) => {
-                if (e.which === 13) {
-                    meta.oldval = e.target.value;
-                    setValue(e.target.value);
-                    submit();
-                }
-            }}
-            styles={{ root: { padding: 10 } }} /> */}
-        
-        
-    </ItemButton>;
-    
-})
+});
