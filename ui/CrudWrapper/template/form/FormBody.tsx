c7097ef62345bb917df63ac952acf7e445ad2ab6
Rizky <rizky@andromedia.co.id>
2020-04-22 12:32:10 +0700

    fix

--- ui/CrudWrapper/template/form/FormBody.tsx
+++ ui/CrudWrapper/template/form/FormBody.tsx
@@ -1,121 +1,157 @@
-import _ from 'lodash';
-import { toJS } from 'mobx';
-import { observer, useObservable } from 'mobx-react-lite';
-import { Label } from 'office-ui-fabric-react/lib/Label';
-import { MessageBar, MessageBarType } from 'office-ui-fabric-react/lib/MessageBar';
-import * as React from 'react';
+import _ from "lodash";
+import { toJS } from "mobx";
+import { observer, useObservable } from "mobx-react-lite";
+import { Label } from "office-ui-fabric-react/lib/Label";
+import {
+  MessageBar,
+  MessageBarType,
+} from "office-ui-fabric-react/lib/MessageBar";
+import * as React from "react";
 
-export default observer(({ parsed, data, errors, fields, formRef, events }: any) => {
+export default observer(
+  ({ parsed, data, errors, fields, formRef, events }: any) => {
     const meta = useObservable({
-        data: toJS(data),
-        errors: toJS(errors) || {},
-        beforeSubmit: _.get(events, 'beforeSubmit'),
-        afterSubmit: _.get(events, 'afterSubmit'),
+      data: toJS(data),
+      errors: toJS(errors) || {},
+      beforeSubmit: _.get(events, "beforeSubmit"),
+      afterSubmit: _.get(events, "afterSubmit"),
     });
 
     React.useEffect(() => {
-        meta.data = data
+      meta.data = data;
     }, [data]);
 
     const errorLen = Object.keys(meta.errors).length;
     const sections = {};
-    fields.columns.forEach(e => {
-        if (e.props.section) {
-            if (!sections[e.props.section]) {
-                sections[e.props.section] = [];
-            }
-
-            sections[e.props.section].push(e);
+    fields.columns.forEach((e) => {
+      if (e.props.section) {
+        if (!sections[e.props.section]) {
+          sections[e.props.section] = [];
         }
+
+        sections[e.props.section].push(e);
+      }
     });
-    const presuf = _.map(_.get(parsed, 'table.head.children'), 'props')
+    const presuf = _.map(_.get(parsed, "table.head.children"), "props");
 
     const renderField = (e, idx, isSection = false) => {
-        if (!e.children) {
-            return <React.Fragment key={idx}>{e}</React.Fragment>;
-        }
-        if (isSection === false && e.props.section) return null;
-        const Field = e.children.type;
-        const ps = _.find(presuf, { path: e.props.path });
-        return <Field
-            {...e.props}
-            {...e.props.children.props}
-            {...e.children.props}
-            prefix={_.get(ps, 'prefix')}
-            suffix={_.get(ps, 'suffix')}
-            value={_.get(meta.data, e.props.path)}
-            errorMessage={meta.errors[e.props.path]}
-            onChange={(v) => {
-                const value = _.get(v, 'target.value', v);
-                _.set(meta.data, e.props.path, value);
-                const onChange = _.get(e, 'props.children.props.onChange');
-                if (onChange) {
-                    onChange(v, meta.data);
-                }
-                else {
-                    const optionsOnChange = _.get(e, 'props.options.onChange');
-                    if (optionsOnChange) optionsOnChange(v, meta.data);
-                }
-            }}
-            key={idx}
-        />;
-    }
+      if (!e.children) {
+        return <React.Fragment key={idx}>{e}</React.Fragment>;
+      }
+      if (isSection === false && e.props.section) return null;
+      const Field = e.children.type;
+      const ps = _.find(presuf, { path: e.props.path });
+      const overridenProps: any = {};
+      const onBlur = _.get(e, "props.options.onBlur");
+      if (onBlur) {
+        overridenProps.onBlur = onBlur;
+      }
+
+      return (
+        <Field
+          {...e.props}
+          {...e.props.children.props}
+          {...e.children.props}
+          {...overridenProps}
+          prefix={_.get(ps, "prefix")}
+          suffix={_.get(ps, "suffix")}
+          value={_.get(meta.data, e.props.path)}
+          errorMessage={meta.errors[e.props.path]}
+          onChange={(v) => {
+            const value = _.get(v, "target.value", v);
+            _.set(meta.data, e.props.path, value);
+            const onChange = _.get(e, "props.children.props.onChange");
+            if (onChange) {
+              onChange(v, meta.data);
+            } else {
+              const optionsOnChange = _.get(e, "props.options.onChange");
+              if (optionsOnChange) optionsOnChange(v, meta.data);
+            }
+          }}
+          key={idx}
+        />
+      );
+    };
 
     formRef.current = meta;
     const sectionKeys = Object.keys(sections);
     const columns = fields.columns;
     if (errorLen > 0) {
-        console.log(toJS(meta.errors));
+      console.log(toJS(meta.errors));
     }
-    return <>
-        {errorLen > 0 && <div style={{ flex: 1, width: '100%', marginBottom: 20 }}>
-            <MessageBar messageBarType={MessageBarType.error} isMultiline={false}>
-                Save Failed. Please fix {errorLen} error below:
-        </MessageBar>
-        </div>
-        }
+    return (
+      <>
+        {errorLen > 0 && (
+          <div style={{ flex: 1, width: "100%", marginBottom: 20 }}>
+            <MessageBar
+              messageBarType={MessageBarType.error}
+              isMultiline={false}
+            >
+              Save Failed. Please fix {errorLen} error below:
+            </MessageBar>
+          </div>
+        )}
         {sectionKeys.map((key) => {
-            return <div key={key} style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'stretch',
-                marginBottom: '30px'
-            }}>
-                <Label style={{
-                    fontSize: 18,
-                    paddingLeft: 5,
-                    fontWeight: 300,
-                    color: '#777',
-                    borderBottom: '1px solid #ccc',
-                    marginBottom: '5px'
-                }}>
-                    {key}
-                </Label>
-                <div style={{
-                    display: 'flex', flex: 1, flexDirection: 'row', flexWrap: 'wrap',
-                    paddingLeft: '15px',
-                }}>
-                    {sections[key].map((e, idx) => renderField(e, idx, true))}
-                </div>
+          return (
+            <div
+              key={key}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "stretch",
+                marginBottom: "30px",
+              }}
+            >
+              <Label
+                style={{
+                  fontSize: 18,
+                  paddingLeft: 5,
+                  fontWeight: 300,
+                  color: "#777",
+                  borderBottom: "1px solid #ccc",
+                  marginBottom: "5px",
+                }}
+              >
+                {key}
+              </Label>
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  paddingLeft: "15px",
+                }}
+              >
+                {sections[key].map((e, idx) => renderField(e, idx, true))}
+              </div>
             </div>
+          );
         })}
 
-        {sectionKeys.length > 0 && <div style={{
-            borderBottom: '1px solid #ccc',
-            width: '100%',
-            marginTop: '20px',
-            marginBottom: '20px'
-        }}></div>}
+        {sectionKeys.length > 0 && (
+          <div
+            style={{
+              borderBottom: "1px solid #ccc",
+              width: "100%",
+              marginTop: "20px",
+              marginBottom: "20px",
+            }}
+          ></div>
+        )}
 
         <div
-            style={{
-                display: 'flex',
-                flexDirection: 'row',
-                paddingLeft: sectionKeys.length > 0 ? 15 : 0,
-                flexWrap: 'wrap'
-            }}>
-            {columns.map((e, idx) => renderField(e, idx, false))}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            paddingLeft: sectionKeys.length > 0 ? 15 : 0,
+            flexWrap: "wrap",
+          }}
+        >
+          {columns.map((e, idx) => renderField(e, idx, false))}
         </div>
         <div style={{ height: 300 }}> </div>
-    </>
-});
+      </>
+    );
+  }
+);
