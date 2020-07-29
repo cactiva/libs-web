c9f5bc570a4caaed63d849d82c69cd79312cd607
Magnificat <hansel.sindu@gmail.com>
2020-07-29 07:42:18 +0700

    getMonthName

--- utils/date.ts
+++ utils/date.ts
@@ -31,6 +31,24 @@ export const dateFormat = (value: any, format?: string) => {
   }
 };
 
-export const validateDate = (value:any) => {
+export const validateDate = (value: any) => {
   return isValid(parseISO(value));
+}
+
+export const getMonthName = (date: any) => {
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+  const monthName = month[date.getMonth()];
+  return monthName;
 }
